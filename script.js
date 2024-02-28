17b2fd3742449be18899b97dc622133868330963
Gautam <gauta.m.endiratta@gmial.com>
2024-02-28 23:30:56 +0530

    Initial Commit

---
+++ script.js
@@ -0,0 +1,16 @@
+console.log("factorial");
+
+
+// using loop method
+let ans=1;
+let number = prompt("Enter number")
+for (i=1; i<=number; i++){
+  ans = ans * i;
+}
+
+// using array and reduce method
+
+
+console.log(ans);
+
+
