4bebd012b1152863095d7efc0713c52fc33c269b
Gautam <gauta.m.endiratta@gmial.com>
2024-02-29 16:22:49 +0530

    Added Method

--- script.js
+++ script.js
@@ -2,15 +2,32 @@ console.log("factorial");
 
 
 // using loop method
-let ans=1;
+
 let number = prompt("Enter number")
-for (i=1; i<=number; i++){
-  ans = ans * i;
+
+let ans=1;
+const factorial = (num) => {
+  for (i=1; i<=num; i++){
+    ans = ans * i;
+  }
 }
+
+factorial(number);
+console.log(ans);
+
 
 // using array and reduce method
 
+let array = [];
+for (let i = 1; i<=number; i++){
+  array.push(i);
+}
 
-console.log(ans);
+const factorial_second_method = (a,b)=>{
+  return a*b;
+}
+const answer = array.reduce(factorial_second_method);
+console.log(answer);
+
 
 
